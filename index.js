a4fee37522cc22a41a28629cabe731a5f1f995da
Naldi Castro Serrano <68132636+NeaCS@users.noreply.github.com>
2023-10-24 09:44:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,16 +34,31 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.put("eventos/:id", async (req, res) => {
+app.put("/eventos/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const Authorization = req.header("Authorization");
-    const token = Authorization.split("Bearer ")[1];
-    console.log(token);
+    console.log("ID recibido:", id);
+
+    const authorization = req.headers["authorization"];
+    if (!authorization) {
+      throw new Error("Authorization header missing");
+    }
+
+    const token = authorization.split("Bearer ")[1];
+    console.log("Token recibido:", token);
+
+    // Aquí puedes agregar el código para obtener el payload de un evento a actualizar.
+    // Por ejemplo:
+    const payload = req.body; // Suponiendo que el payload se envía en el cuerpo de la solicitud.
+
+    // Ahora puedes usar 'payload' para actualizar el evento.
+
+    // Responde con un mensaje de éxito o cualquier otra lógica necesaria.
+    res.status(200).send("Evento actualizado exitosamente");
   } catch (error) {
-    res.status(error.code || 500).send(error);
+    res.status(error.code || 500).send(error.message);
   }
-})
+});
 
 app.delete("/eventos/:id", async (req, res) => {
   try {
